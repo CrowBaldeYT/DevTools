7585750f9bd3c46ac584afcc0507b5be25e21c69
CrowBaldeYT <75171427+CrowBaldeYT@users.noreply.github.com>
2020-12-30 23:07:49 +0100

    Update Main.php

--- resources/plugin_skeleton/Main.php
+++ resources/plugin_skeleton/Main.php
@@ -5,7 +5,12 @@ declare(strict_types=1);
 #%{Namespace}
 
 use pocketmine\plugin\PluginBase;
+use pocketmine\event\Listener;
+use pocketmine\Server;
 
-class Main extends PluginBase{
+class Main extends PluginBase implements Listener{
 
+  public function onEnable(){
+   $this->getServer()->getLogger()->info("Loaded Succesfully our Plugin");
+  }
 }
